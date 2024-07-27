913b1cab8b21cebccab9a96c393c889a3f40be80
streamich <streamich@gmail.com>
2024-07-27 10:51:51 +0200

    style: 💄 run Prettier

--- src/compression/gzip.ts
+++ src/compression/gzip.ts
@@ -1,8 +1,10 @@
 import {fromStream} from '../streams/fromStream';
 import {toStream} from '../streams/toStream';
 
-const pipeThrough = async (data: Uint8Array, transform: ReadableWritablePair<Uint8Array, Uint8Array>): Promise<Uint8Array> =>
-  await fromStream(toStream(data).pipeThrough<Uint8Array>(transform));
+const pipeThrough = async (
+  data: Uint8Array,
+  transform: ReadableWritablePair<Uint8Array, Uint8Array>,
+): Promise<Uint8Array> => await fromStream(toStream(data).pipeThrough<Uint8Array>(transform));
 
 export const gzip = async (data: Uint8Array): Promise<Uint8Array> =>
   await pipeThrough(data, new CompressionStream('gzip'));
